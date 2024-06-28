ecae2f2e7256f1f4681c7b41cc0fe8313b0f7db9
Lewis Jubb <lewis.jubb@xe.com>
2024-06-28 16:36:05 +0100

    Add tests for monad laws

--- Testys/StateTests.cs
+++ Testys/StateTests.cs
@@ -1,3 +1,4 @@
+using FluentAssertions;
 using TestBed;
 using Xunit;
 
@@ -35,13 +36,13 @@ public sealed class StateTests
     [InlineData("quux", 3)]
     public void SecondFunctorLaw(string txt, int i)
     {
-        Func<string, int> g = x => x.Length;
-        Func<int, bool> f = x => x % 2 == 0;
+        bool Func(int x) => x % 2 == 0;
 
         var s = VowelExpander(txt);
         Assert.Equal(
-            s.Select(g).Select(f).Run(i),
-            s.Select(x => f(g(x))).Run(i));
+            s.Select((Func<string, int>)Func1).Select(Func).Run(i),
+            s.Select(x => Func(Func1(x))).Run(i));
+        int Func1(string x) => x.Length;
     }
     
     [Theory]
@@ -56,6 +57,79 @@ public sealed class StateTests
         var s = VowelExpander(txt);
         var t = s.Run(count);
         Assert.Equal((expected, count + 1), t);
+    }
+    
+    [Theory]
+    [InlineData(1)]
+    [InlineData(100)]
+    [InlineData(53)]
+    public void Should_Obey_Monad_Left_Identity_Law(int a)
+    {
+        State<int, string> F(int i) => State.Return<int, string>($"{i}");
+
+        // binding on a value wrapped via unit is the same as calling the thing directly
+        // unit is on the left of bind
+        State.Return<int, int>(a).SelectMany(F)(1).Should().Be(F(a)(1));
+    }
+    
+    [Theory]
+    [InlineData("one")]
+    [InlineData("some")]
+    [InlineData("test")]
+    public void Should_Obey_Monad_Right_Identity_Law(string a)
+    {
+        State<int, int> F(string s) => State.Return<int, int>(s.Length);
+        var m = F(a);
+
+        // binding unit on the result of calling f is the same as calling the thing directly
+        // unit is on the right of bind - unit is basically the id func for the monad
+        m.SelectMany(State.Return<int,int>)(1).Should().Be(m(1));
+    }
+    
+    [Theory]
+    [InlineData(0)]
+    [InlineData(1)]
+    [InlineData(2)]
+    public void Should_Obey_The_Associativity_Law(double a)
+    {
+        State<string, bool> F(double i) => State.Return<string, bool>(i % 2 == 0);
+        State<string, string> G(bool b) => State.Return<string,string>(b.ToString());
+        State<string, int> H(string s) => State.Return<string,int>(s.Length);
+        var m = F(a);
+ 
+        // the order of grouping binds together is like brackets for adding (a + b) + c == a + (b + c)
+        m.SelectMany(G).SelectMany(H)("").Should().Be(m.SelectMany(x => G(x).SelectMany(H))(""));
+    }
+
+    [Fact]
+    public void BiMap()
+    {
+        var a = State.Return<string, int>(10);
+        var b = State.Return<string, int>(2);
+
+        var s = a.BiMap(b, (x, y) => x / y);
+        var ss = s.BiMap(b, (x, y) => x * y)("");
+        ss.Value.Should().Be(10);
+    }
+    
+    [Fact]
+    public void ModifyExample()
+    {
+        var x = State.Modify((int i) => i + 1);
+        var actual = x.Run(1);
+        Assert.Equal(2, actual.State);
+    }
+
+    [Fact]
+    public void ModifyExample2()
+    {
+        var initialState = new Machine(true, 10, 0);
+        Input[] input = [Input.Coin, Input.Turn, Input.Turn, Input.Turn];
+        var stateMachine = Machines.SimulateMachine(input);
+
+        var result = stateMachine.Run(initialState);
+        
+        result.Value.Should().Be((9, 1));
     }
 
     private State<int, string> VowelExpander(string text) =>
